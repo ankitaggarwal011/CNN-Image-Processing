8b448789db192b82916ba59b7e9d97b0eca89494
Patrick Bollinger <owner@pjbollinger.com>
2016-10-13 20:53:11 -0400

    PEP8 Compliance

--- pycnn.py
+++ pycnn.py
@@ -135,16 +135,16 @@ class pycnn(object):
         z0 = u * initialcondition
         Bu = sig.convolve2d(u, tempB, 'same')
         z0 = z0.flatten()
-        #ode = sint.ode(self.f,jac=None)\
-        #    .set_integrator("lsoda", max_step=1000)\
-        #    .set_initial_value(z0)\
-        #    .set_f_params(Ib, Bu, tempA)
-        #ode_result = ode.integrate(t)
+        # ode = sint.ode(self.f,jac=None)\
+        #     .set_integrator("lsoda", max_step=1000)\
+        #     .set_initial_value(z0)\
+        #     .set_f_params(Ib, Bu, tempA)
+        # ode_result = ode.integrate(t)
         ode_result = sint.odeint(
             self.f, z0, t, args=(Ib, Bu, tempA), mxstep=1000)
-        #if ode.successful():
-        #    print("It worked!")
-        #else:
+        # if ode.successful():
+        #     print("It worked!")
+        # else:
         #    print("Nope. Didn't work.")
         z = self.cnn(ode_result)
         l = z[z.shape[0] - 1, :].reshape((self.n, self.m))
