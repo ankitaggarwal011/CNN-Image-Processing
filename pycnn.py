9582e7bb4371bda9007dab32f681aeea83d96049
Patrick Bollinger <owner@pjbollinger.com>
2016-10-13 21:06:48 -0400

    Trying OO ode to see if it works on Travis CI

--- pycnn.py
+++ pycnn.py
@@ -135,13 +135,13 @@ class pycnn(object):
         z0 = u * initialcondition
         Bu = sig.convolve2d(u, tempB, 'same')
         z0 = z0.flatten()
-        # ode = sint.ode(self.f,jac=None)\
-        #     .set_integrator("lsoda", max_step=1000)\
-        #     .set_initial_value(z0)\
-        #     .set_f_params(Ib, Bu, tempA)
-        # ode_result = ode.integrate(t)
-        ode_result = sint.odeint(
-            self.f, z0, t, args=(Ib, Bu, tempA), mxstep=1000)
+        ode = sint.ode(self.f, jac=None)\
+            .set_integrator("lsoda", max_step=1000)\
+            .set_initial_value(z0)\
+            .set_f_params(Ib, Bu, tempA)
+        ode_result = ode.integrate(t)
+        # ode_result = sint.odeint(
+        #     self.f, z0, t, args=(Ib, Bu, tempA), mxstep=1000)
         # if ode.successful():
         #     print("It worked!")
         # else:
